603cd01651bd78ed78e0cae066dff28188179c99
wangxy <1187798791@qq.com>
2019-12-25 10:24:37 +0800

    对话框实例demo完善

--- example/BlendModalDemo.js
+++ example/BlendModalDemo.js
@@ -16,8 +16,56 @@ class BlendModalDemo extends React.Component {
      */
     renderAlertContent(content) {
         return (
-            <Text style={{color: '#333333', fontSize: 16}}>{content}</Text>
+            <Text style={{color: '#333333', fontSize: 15}}>{content}</Text>
         );
+    }
+
+    /**
+     * 编辑框
+     * @param content
+     */
+    renderEditContent(content) {
+        return (
+            <View style={{flex:1}}>
+                <Text style={{color: '#333333', fontSize: 16}}>{content}</Text>
+            </View>
+        );
+    }
+
+    /**
+     * 选择框
+     * @param content
+     */
+    renderSelectedContent(content) {
+        return (
+            <TouchableOpacity activeOpacity={0.5} onPress={this.onSelected.bind(this)}>
+                <Text style={{color: '#222222', fontSize: 15}}>{content}</Text>
+            </TouchableOpacity>
+        );
+    }
+
+    /**
+     * 选择事件
+     */
+    onSelected() {
+        this.AecAlertMessageModal.showModal(3, this.renderAlertContent("数据处理中..."));
+        setTimeout(() => {
+            this.AecAlertMessageModal.closeModal();
+        }, 1500);
+    }
+
+    /**
+     * 确定事件
+     */
+    onOkModal() {
+
+    }
+
+    /**
+     * 对话框关闭事件
+     */
+    onHidenModal() {
+
     }
 
     /**
@@ -32,14 +80,26 @@ class BlendModalDemo extends React.Component {
                     <Text style={{color: '#222222', fontSize: 15}}>提示框</Text>
                 </TouchableOpacity>
                 <TouchableOpacity activeOpacity={0.5} style={styles.rows}
-                                  onPress={() => this.AecAlertMessageModal.showModal(3, this.renderAlertContent("加载中..."))}>
-                    <Text style={{color: '#222222', fontSize: 15}}>加载框</Text>
+                                  onPress={() => this.AecAlertMessageModal.showModal(1, this.renderEditContent("编辑框测试"))}>
+                    <Text style={{color: '#222222', fontSize: 15}}>编辑框</Text>
+                </TouchableOpacity>
+                <TouchableOpacity activeOpacity={0.5} style={styles.rows}
+                                  onPress={() => this.AecAlertMessageModal.showModal(2, this.renderSelectedContent("选择框测试"))}>
+                    <Text style={{color: '#222222', fontSize: 15}}>选择框</Text>
+                </TouchableOpacity>
+                <TouchableOpacity activeOpacity={0.5} style={styles.rows}
+                                  onPress={() => this.AecAlertMessageModal.showModal(4, this.renderAlertContent("对话框测试"))}>
+                    <Text style={{color: '#222222', fontSize: 15}}>对话框</Text>
+                </TouchableOpacity>
+                <TouchableOpacity activeOpacity={0.5} style={styles.rows}
+                                  onPress={() => this.AecAlertMessageModal.showModal(5, this.renderAlertContent("Sheet框测试"))}>
+                    <Text style={{color: '#222222', fontSize: 15}}>Sheet框</Text>
                 </TouchableOpacity>
                 <TouchableOpacity activeOpacity={0.5} style={styles.rows}
                                   onPress={() => this.AecAlertMessageModal.closeModal()}>
                     <Text style={{color: '#222222', fontSize: 15}}>关闭</Text>
                 </TouchableOpacity>
-                <AecAlertMessageModal ref={o => this.AecAlertMessageModal = o} okModal={null} hidenModal={null}
+                <AecAlertMessageModal ref={o => this.AecAlertMessageModal = o} okModal={this.onOkModal.bind(this)} hidenModal={this.onHidenModal.bind(this)}
                                       closeText={'取消'} okText={'提交'} closeBtn={true} RatioWidth={0.8}/>
             </View>
         );
